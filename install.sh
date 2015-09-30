08bcdc7cc8791f109d70a06a959cbf42d9458330
Ubuntu <ubuntu@w1.firestart.me>
2015-09-30 04:48:16 +0000

    Minor fix to install.sh.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mkdir -p /opt
 rm -rf /opt/nginx-cfg
+mkdir -p /opt/nginx-cfg
 cp -r * /opt/nginx-cfg
 ln -fs /opt/nginx-cfg/nginx-cfg.pl /usr/local/bin/nginx-cfg
 chmod ugo+rx /usr/local/bin/nginx-cfg
