464316968bcba224735dbeb5efa7c73116700923
Troy Conrad <troy.conrad@lbox.com>
2015-10-21 14:04:48 -0700

    Updated install.sh with variable name bug fixes and re-added dependency install.

--- install.sh
+++ install.sh
@@ -1,22 +1,29 @@
 #!/bin/sh
 
-# install.sh v1.0.0 21Oct2015 (Great Scott!)
+# install.sh v1.0.1 21Oct2015 (Great Scott!)
 
 # Linux or bust!
 osName=`uname`
-if [[ "$osName" != 'Linux' ]]; then
-	echo "Sorry, this script only works on Linux."
-	exit
+if [ "$osName" != 'Linux' ]; then
+        echo "Sorry, this script only works on Linux."
+        exit
 fi
 
-scriptName='nginx-cfg'
-installPath='/opt/$scriptname'
-binPath='/usr/local/bin/$scriptname'
+echo "Installing dependencies..."
 
-echo "Installing $scriptname..."
+apt-get install libtemplate-perl -y
 
+scriptName="nginx-cfg"
+installPath="/opt/$scriptName"
+binPath="/usr/local/bin/$scriptName"
+
+echo "Installing $scriptName..."
+
+rm -rf $installPath
 mkdir -p $installPath
-rm -rf $installPath/*
 cp -r * $installPath
-ln -fs $installPath/$scriptname $binPath
+ln -fs $installPath/$scriptName $binPath
 chmod ugo+rx $binPath
+
+echo "Installation complete.\n"
+
